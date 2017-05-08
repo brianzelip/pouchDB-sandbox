7acc23cefcb5f99126d4deaa58ba4e5ab27d78ac
Brian Zelip <bzelip@gmail.com>
2017-05-08 16:29:10 -0400

    small update

--- index.js
+++ index.js
@@ -23,14 +23,22 @@ request("http://elm.umaryland.edu/", function(error, response, body) {
   }
   console.log("Status code: " + response.statusCode);
 
-  const ELM_HTML = cheerio.load(body);
+  const $ = cheerio.load(body);
 
-  let articles = ELM_HTML('#mpcth_page_articles > article');
-  let ids = articles.each( (index) => { console.log('this is', this) } );
-  console.log('typeof(body)', typeof(body));
-  console.log('typeof(ELM_HTML)', typeof(ELM_HTML));
-  console.log('typeof(articles)', typeof(articles));
-  fs.appendFileSync('articles.js', articles);
+  let articleIds = [];
+
+  $('#mpcth_page_articles > article').each(function(index) {
+    articleIds.push($(this).attr('id'));
+    console.log('articleIds', articleIds);
+  });
+
+  console.log('articleIds.legnth', articleIds.length);
+
+  // console.log('typeof(body)', typeof(body));
+  // console.log('typeof($)', typeof($));
+  // console.log('typeof(articleIds)', typeof(articleIds));
+  // console.log('articleIds', articleIds);
+  // fs.appendFileSync('articles.js', articles);
 
   // let articleIds = articles.map( (article) => console.log(this) );
 
